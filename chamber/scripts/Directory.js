96ff3b49a5885bbecbdef765f8a9d977e09a87a7
Vermont Garcia <vermontgarcia@gmail.com>
2025-03-22 23:30:31 -0600

    Directory refactor

--- chamber/scripts/Directory.js
+++ chamber/scripts/Directory.js
@@ -1,10 +1,4 @@
-const parseJson = (res) => {
-  if (res.ok) {
-    return res.json();
-  } else {
-    throw new Error('Bad Response');
-  }
-};
+import { parseJson } from './utils.js';
 
 const businnessCardTemplate = (business) => {
   const {
@@ -48,3 +42,25 @@ export default class Directory {
     this.renderList(this.data);
   }
 }
+
+// DOM Manipulation
+document.getElementById('icon-btn-grid').addEventListener('click', () => {
+  document.querySelector('#icon-btn-grid i').classList.add('selected');
+  document.querySelector('#icon-btn-list i').classList.remove('selected');
+
+  const directoryContainer = document.getElementById('directory-container');
+  directoryContainer.classList.remove('list');
+  directoryContainer.classList.add('grid');
+});
+
+document.getElementById('icon-btn-list').addEventListener('click', () => {
+  document.querySelector('#icon-btn-list i').classList.add('selected');
+  document.querySelector('#icon-btn-grid i').classList.remove('selected');
+  const directoryContainer = document.getElementById('directory-container');
+  directoryContainer.classList.remove('grid');
+  directoryContainer.classList.add('list');
+});
+
+//
+const directory = new Directory(document.getElementById('directory-container'));
+directory.init();
