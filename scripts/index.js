503c6d4cf14e04ec7ca777560251f057f32b31ff
Vermont Garcia <vermontgarcia@gmail.com>
2025-04-11 11:09:33 -0600

    Modals

--- scripts/index.js
+++ scripts/index.js
@@ -1,103 +1,103 @@
-console.log("Hello class from console!");
+console.log('Hello class from console!');
 
 // Content
 const courses = [
   {
-    subject: "CSE",
+    subject: 'CSE',
     number: 110,
-    title: "Introduction to Programming",
+    title: 'Introduction to Programming',
     credits: 2,
-    certificate: "Web and Computer Programming",
+    certificate: 'Web and Computer Programming',
     description:
-      "This course will introduce students to programming. It will introduce the building blocks of programming languages (variables, decisions, calculations, loops, array, and input/output) and use them to solve problems.",
-    technology: ["Python"],
+      'This course will introduce students to programming. It will introduce the building blocks of programming languages (variables, decisions, calculations, loops, array, and input/output) and use them to solve problems.',
+    technology: ['Python'],
     completed: true,
   },
   {
-    subject: "WDD",
+    subject: 'WDD',
     number: 130,
-    title: "Web Fundamentals",
+    title: 'Web Fundamentals',
     credits: 2,
-    certificate: "Web and Computer Programming",
+    certificate: 'Web and Computer Programming',
     description:
-      "This course introduces students to the World Wide Web and to careers in web site design and development. The course is hands on with students actually participating in simple web designs and programming. It is anticipated that students who complete this course will understand the fields of web design and development and will have a good idea if they want to pursue this degree as a major.",
-    technology: ["HTML", "CSS"],
+      'This course introduces students to the World Wide Web and to careers in web site design and development. The course is hands on with students actually participating in simple web designs and programming. It is anticipated that students who complete this course will understand the fields of web design and development and will have a good idea if they want to pursue this degree as a major.',
+    technology: ['HTML', 'CSS'],
     completed: true,
   },
   {
-    subject: "CSE",
+    subject: 'CSE',
     number: 111,
-    title: "Programming with Functions",
+    title: 'Programming with Functions',
     credits: 2,
-    certificate: "Web and Computer Programming",
+    certificate: 'Web and Computer Programming',
     description:
-      "CSE 111 students become more organized, efficient, and powerful computer programmers by learning to research and call functions written by others; to write, call , debug, and test their own functions; and to handle errors within functions. CSE 111 students write programs with functions to solve problems in many disciplines, including business, physical science, human performance, and humanities.",
-    technology: ["Python"],
+      'CSE 111 students become more organized, efficient, and powerful computer programmers by learning to research and call functions written by others; to write, call , debug, and test their own functions; and to handle errors within functions. CSE 111 students write programs with functions to solve problems in many disciplines, including business, physical science, human performance, and humanities.',
+    technology: ['Python'],
     completed: true,
   },
   {
-    subject: "CSE",
+    subject: 'CSE',
     number: 210,
-    title: "Programming with Classes",
+    title: 'Programming with Classes',
     credits: 2,
-    certificate: "Web and Computer Programming",
+    certificate: 'Web and Computer Programming',
     description:
-      "This course will introduce the notion of classes and objects. It will present encapsulation at a conceptual level. It will also work with inheritance and polymorphism.",
-    technology: ["C#"],
+      'This course will introduce the notion of classes and objects. It will present encapsulation at a conceptual level. It will also work with inheritance and polymorphism.',
+    technology: ['C#'],
     completed: true,
   },
   {
-    subject: "WDD",
+    subject: 'WDD',
     number: 131,
-    title: "Dynamic Web Fundamentals",
+    title: 'Dynamic Web Fundamentals',
     credits: 2,
-    certificate: "Web and Computer Programming",
+    certificate: 'Web and Computer Programming',
     description:
-      "This course builds on prior experience in Web Fundamentals and programming. Students will learn to create dynamic websites that use JavaScript to respond to events, update content, and create responsive user experiences.",
-    technology: ["HTML", "CSS", "JavaScript"],
+      'This course builds on prior experience in Web Fundamentals and programming. Students will learn to create dynamic websites that use JavaScript to respond to events, update content, and create responsive user experiences.',
+    technology: ['HTML', 'CSS', 'JavaScript'],
     completed: true,
   },
   {
-    subject: "WDD",
+    subject: 'WDD',
     number: 231,
-    title: "Frontend Web Development I",
+    title: 'Frontend Web Development I',
     credits: 2,
-    certificate: "Web and Computer Programming",
+    certificate: 'Web and Computer Programming',
     description:
-      "This course builds on prior experience with Dynamic Web Fundamentals and programming. Students will focus on user experience, accessibility, compliance, performance optimization, and basic API usage.",
-    technology: ["HTML", "CSS", "JavaScript"],
+      'This course builds on prior experience with Dynamic Web Fundamentals and programming. Students will focus on user experience, accessibility, compliance, performance optimization, and basic API usage.',
+    technology: ['HTML', 'CSS', 'JavaScript'],
     completed: false,
   },
 ];
 
 const courseWork = [
   {
-    name: "Course Home Page",
-    status: "current",
+    name: 'Course Home Page',
+    status: 'current',
   },
   {
-    name: "Chamber Directory Page",
-    status: "pending",
+    name: 'Chamber Directory Page',
+    status: 'pending',
   },
   {
-    name: "Chamber Home Page",
-    status: "pending",
+    name: 'Chamber Home Page',
+    status: 'pending',
   },
   {
-    name: "Chamber Join Page",
-    status: "pending",
+    name: 'Chamber Join Page',
+    status: 'pending',
   },
   {
-    name: "Project Proposal",
-    status: "pending",
+    name: 'Project Proposal',
+    status: 'pending',
   },
   {
-    name: "Project Site Plan",
-    status: "pending",
+    name: 'Project Site Plan',
+    status: 'pending',
   },
   {
-    name: "Project Completion",
-    status: "pending",
+    name: 'Project Completion',
+    status: 'pending',
   },
 ];
 
@@ -114,52 +114,82 @@ const getLastUpdate = () => {
 };
 
 const handleMenu = () => {
-  const menu = document.getElementById("menu");
-  const icon = document.getElementById("menu-icon");
-  if (menu.classList.contains("menu-open")) {
-    menu.classList.remove("menu-open");
-    icon.innerHTML = "menu";
+  const menu = document.getElementById('menu');
+  const icon = document.getElementById('menu-icon');
+  if (menu.classList.contains('menu-open')) {
+    menu.classList.remove('menu-open');
+    icon.innerHTML = 'menu';
   } else {
-    menu.classList.add("menu-open");
-    icon.innerHTML = "close";
+    menu.classList.add('menu-open');
+    icon.innerHTML = 'close';
   }
+};
+
+const displayCourseDetails = ({
+  subject,
+  number,
+  title,
+  credits,
+  certificate,
+  description,
+  technology,
+}) => {
+  const modal = document.getElementById('course-details');
+  modal.innerHTML = `
+    <button id="close-modal">X</button>
+    <h2>${subject} ${number}</h2>
+    <div>
+      <h3>${title}</h3>
+      <p><strong>Credits:</strong> ${credits}</p>
+      <p><strong>Certificate:</strong> ${certificate}</p>
+      <p>${description}</p>
+      <p><strong>Technologies:</strong> ${technology.join(', ')}</p>
+    </div>
+  `;
+  modal.showModal();
+  document.getElementById('close-modal').addEventListener('click', () => {
+    modal.close();
+  });
 };
 
 /** DOM Manipulation */
 
 // Main: Course Work Section
-document.getElementById("course-work").append(
+document.getElementById('course-work').append(
   ...courseWork.map((activity) => {
     // Course Work List elements
-    const li = document.createElement("li");
+    const li = document.createElement('li');
     li.innerHTML = activity.name;
     return li;
   })
 );
 
 // Main: Courses Section
-const setCourses = (set = "all") => {
+const setCourses = (set = 'all') => {
   let newCoursesSet = [...courses];
-  if (set !== "all") {
+  if (set !== 'all') {
     newCoursesSet = courses.filter(
       (course) => course.subject.toLowerCase() === set
     );
   }
 
-  const coursesContainer = document.getElementById("certificate-courses");
-  coursesContainer.innerHTML = "";
+  const coursesContainer = document.getElementById('certificate-courses');
+  coursesContainer.innerHTML = '';
 
   coursesContainer.append(
     ...newCoursesSet.map((course) => {
       // Courses Elements
-      const div = document.createElement("div");
+      const div = document.createElement('div');
       div.innerHTML = `${course.subject} ${course.number}`;
-      div.classList.add(course.completed ? "completed" : "current");
+      div.classList.add(course.completed ? 'completed' : 'current');
+      div.addEventListener('click', () => {
+        displayCourseDetails(course);
+      });
       return div;
     })
   );
 
-  document.getElementById("total-credits").innerHTML = newCoursesSet.reduce(
+  document.getElementById('total-credits').innerHTML = newCoursesSet.reduce(
     (credits, course) => credits + course.credits,
     0
   );
@@ -168,7 +198,7 @@ const setCourses = (set = "all") => {
 setCourses();
 
 // Footer
-document.getElementById("current-year").innerHTML = getYear();
+document.getElementById('current-year').innerHTML = getYear();
 document.getElementById(
-  "last-modified"
+  'last-modified'
 ).innerHTML = `Last Modification: ${getLastUpdate()}`;
