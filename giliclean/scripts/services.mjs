b85597458f98db805e38c66f2b8c92c2f29c2e7f
Vermont Garcia <vermontgarcia@gmail.com>
2025-04-16 23:10:04 -0600

    Services

--- giliclean/scripts/services.mjs
+++ giliclean/scripts/services.mjs
@@ -1,8 +1,32 @@
 import Header from '../components/Header.mjs';
-import { services } from './helpers/consts.mjs';
 import { qs } from './helpers/utils.mjs';
+import { services } from './content.js';
 
 const container = qs('header');
 const header = new Header({ wayfinding: services, container });
 
 header.init();
+
+const servicesAllPageSection = document.querySelector(
+  '#services-all .container'
+);
+
+if (servicesAllPageSection) {
+  const cardsContainer = document.createElement('div');
+  cardsContainer.setAttribute('class', 'cards-container');
+  services.forEach(({ name, description }) => {
+    // Create Services Cards and add them to the DOM
+    const cardDiv = document.createElement('div');
+    cardDiv.setAttribute('class', 'card');
+    const cardH3 = document.createElement('h3');
+    const cardP = document.createElement('p');
+
+    cardH3.innerHTML = name;
+    cardP.innerHTML = description;
+
+    cardDiv.append(cardH3, cardP);
+
+    cardsContainer.appendChild(cardDiv);
+  });
+  servicesAllPageSection.append(cardsContainer);
+}
